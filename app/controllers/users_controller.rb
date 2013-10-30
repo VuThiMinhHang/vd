b2dad7a78b11fc073fbc9df3ec3b73f1c926abcf
Your Name <your.email@example.com>
2013-10-30 08:39:48 +0700

    bibi

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @books = @user.books.paginate(page: params[:page], per_page: 5)
+    @books = @user.books.paginate(page: params[:page], per_page: 3)
   end
 
   def can_show?
