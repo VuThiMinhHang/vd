9f8c78b2f467937f834d8b4c77feb698f715de6e
Your Name <your.email@example.com>
2013-10-30 08:51:10 +0700

    bibi last

--- app/controllers/my_pages_controller.rb
+++ app/controllers/my_pages_controller.rb
@@ -2,7 +2,7 @@ class MyPagesController < ApplicationController
   def home
     if signed_in?
       @book  = current_user.books.build
-      @feed_items = current_user.feed.paginate(page: params[:page], per_page: 10)
+      @feed_items = current_user.feed.paginate(page: params[:page], per_page: 3)
     end
    end
  end
